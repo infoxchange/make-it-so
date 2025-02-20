755d3f2d31c44a5846d4d9bb751bbf87a3b6581f
Callum Gare <cgare@infoxchange.org>
2025-02-20 16:24:36 +1100

    feat: add new env vars to deployConfig

--- src/deployConfig.ts
+++ src/deployConfig.ts
@@ -1,23 +1,25 @@
-type IxDeployConfig =
-  | {
-      isIxDeploy: true;
-      appName: string;
-      environment: string;
-      workloadGroup: string;
-      primaryAwsRegion: string;
-      siteDomains: string[];
-    }
-  | {
-      isIxDeploy: false;
-      appName: undefined;
-      environment: undefined;
-      workloadGroup: undefined;
-      primaryAwsRegion: undefined;
-      siteDomains: [];
-    };
+type SharedDeployConfig = {
+  appName: string;
+  environment: string;
+  workloadGroup: string;
+  primaryAwsRegion: string;
+  siteDomains: string[];
+  isInternalApp: boolean;
+  deploymentType: string;
+  sourceCommitRef: string;
+  sourceCommitHash: string;
+  deployTriggeredBy: string;
+  smtpHost: string;
+  smtpPort: string;
+  clamavUrl: string;
+};
+
+export type IxDeployConfig =
+  | ({ isIxDeploy: true } & SharedDeployConfig)
+  | ({ isIxDeploy: false } & Partial<SharedDeployConfig>);
 
 export default {
-  isIxDeploy: Boolean(process.env.IX_DEPLOYMENT),
+  isIxDeploy: process.env.IX_DEPLOYMENT?.toLowerCase() === "true",
   appName: process.env.IX_APP_NAME,
   environment: process.env.IX_ENVIRONMENT,
   workloadGroup: process.env.IX_WORKLOAD_GROUP,
@@ -25,4 +27,12 @@ export default {
   siteDomains: (process.env.IX_SITE_DOMAINS || "")
     .split(",")
     .map((domain) => domain.trim()),
+  isInternalApp: process.env.IX_INTERNAL_APP?.toLowerCase() === "true",
+  deploymentType: process.env.IX_DEPLOYMENT_TYPE,
+  sourceCommitRef: process.env.IX_SOURCE_COMMIT_REF,
+  sourceCommitHash: process.env.IX_SOURCE_COMMIT_HASH,
+  deployTriggeredBy: process.env.IX_DEPLOY_TRIGGERED_BY,
+  smtpHost: process.env.SMTP_HOST,
+  smtpPort: process.env.SMTP_PORT,
+  clamavUrl: process.env.CLAMAV_URL,
 } as IxDeployConfig;
