f72e26d3fcaad6acd4fad1cbb8e16bdf4e1d13d9
Callum Gare <cgare@infoxchange.org>
2025-03-28 13:54:44 +1100

    feat: add IxStaticSite

--- src/cdk-constructs/index.ts
+++ src/cdk-constructs/index.ts
@@ -2,6 +2,7 @@ export * from "./IxVpcDetails.js";
 export * from "./IxCertificate.js";
 export * from "./IxDnsRecord.js";
 export * from "./IxNextjsSite.js";
+export * from "./IxStaticSite.js";
 export * from "./IxElasticache.js";
 export * from "./IxApi.js";
 export * from "./IxQuicksightWorkspace.js";
