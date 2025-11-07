db9e4a395224a11c5618b0caff11c7761d4ba291
Callum Gare <cgare@infoxchange.org>
2025-11-07 15:23:01 +1100

    feat: make auth settable as a site prop

--- src/cdk-constructs/SiteOidcAuth/auth-check-handler-body.ts
+++ src/cdk-constructs/SiteOidcAuth/auth-check-handler-body.ts
@@ -1,11 +1,37 @@
 // Based off: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/example_cloudfront_functions_kvs_jwt_verify_section.html
-// Note that as a CloudFront Function, this code has limitations compared to a Lambda@Edge function.
-// For example, no external libraries can be used, and the runtime is more limited.
-import crypto from "crypto";
-import cf from "cloudfront";
+// Note that as a CloudFront Function, this code has limitations compared to a Lambda@Edge function. For example, no
+// external libraries can be used, and the runtime is more limited. Because SST v2's SsrSite construct uses JS v1.0
+// runtime for CloudFront Functions, this code must also be compatible with that runtime. Also this is used in the body
+// of a function where the variables event and request are already defined.
 
-const kvsKey = "__placeholder-for-jwt-secret-key__";
+declare const request: AWSCloudFrontFunction.Request;
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires -- v1 runtime for CloudFront Functions do not support import statements
+const crypto: typeof import("crypto") = require("crypto");
+
+const jwtSecret = "__placeholder-for-jwt-secret__";
 const authRoutePrefix = "__placeholder-for-auth-route-prefix__";
+
+// Set to true to enable console logging
+const loggingEnabled = false;
+
+const log: typeof console.log = function () {
+  if (!loggingEnabled) return;
+
+  // CloudFront Function runtime only prints first argument passed to console.log so add other args to the first one if given.
+  // eslint-disable-next-line prefer-rest-params -- We can't use spread or rest parameters in CloudFront Functions
+  let message = arguments[0];
+  if (arguments.length > 1) {
+    const otherArgs = [];
+    for (let i = 1; i < arguments.length; i++) {
+      // eslint-disable-next-line prefer-rest-params
+      otherArgs[i - 1] = arguments[i];
+    }
+
+    message += " - additional args: " + JSON.stringify(otherArgs);
+  }
+  console.log(message);
+};
 
 //Response when JWT is not valid.
 const redirectResponse = {
@@ -14,9 +40,6 @@ const redirectResponse = {
     location: { value: `${authRoutePrefix}/oidc/authorize` },
   },
 };
-
-// Set to true to enable console logging
-const loggingEnabled = false;
 
 function jwtDecode(token: string, key: string, noVerify?: boolean) {
   // check segments
@@ -96,62 +119,20 @@ function _base64urlDecode(str: string) {
   return Buffer.from(str, "base64url").toString();
 }
 
-async function handler(event: AWSCloudFrontFunction.Event) {
-  const request = event.request;
-  const secret_key = await getSecret();
+const jwtToken =
+  request.cookies["auth-token"] && request.cookies["auth-token"].value;
 
-  if (!secret_key) {
-    // It's not possible for us to validate requests without the secret key so we have no choice but to block all requests.
-    throw new Error("Error retrieving JWT secret key");
-  }
-
-  const jwtToken =
-    request.cookies["auth-token"] && request.cookies["auth-token"].value;
-
-  if (!jwtToken) {
-    log("Error: No JWT in the cookies");
-    return redirectResponse;
-  }
-  try {
-    jwtDecode(jwtToken, secret_key);
-  } catch (e) {
-    log(e);
-    return redirectResponse;
-  }
-
-  log("Valid JWT token");
-  return request;
+if (!jwtToken) {
+  log("Error: No JWT in the cookies");
+  // @ts-expect-error -- This code is added to a function body so we can use return here but typescript doesn't know that.
+  return redirectResponse;
+}
+try {
+  jwtDecode(jwtToken, jwtSecret);
+} catch (e) {
+  log(e);
+  // @ts-expect-error -- This code is added to a function body so we can use return here but typescript doesn't know that.
+  return redirectResponse;
 }
 
-// Get secret from key value store
-async function getSecret() {
-  try {
-    const kvsHandle = cf.kvs();
-    return await kvsHandle.get(kvsKey);
-  } catch (err) {
-    log(`Error reading value for key: ${kvsKey}, error: ${err}`);
-    return null;
-  }
-}
-
-const log: typeof console.log = function () {
-  if (!loggingEnabled) return;
-
-  // CloudFront Function runtime only prints first argument passed to console.log so add other args to the first one if given.
-  // eslint-disable-next-line prefer-rest-params -- We can't use spread or rest parameters in CloudFront Functions
-  let message = arguments[0];
-  if (arguments.length > 1) {
-    const otherArgs = [];
-    for (let i = 1; i < arguments.length; i++) {
-      // eslint-disable-next-line prefer-rest-params
-      otherArgs[i - 1] = arguments[i];
-    }
-
-    message += " - additional args: " + JSON.stringify(otherArgs);
-  }
-  console.log(message);
-};
-
-// This serves no purpose other than to make TypeScript and eslint happy by showing that that handler is used. We can't
-// export handler as an alterative because CloudFront Functions don't support exports.
-handler;
+log("Valid JWT token");
