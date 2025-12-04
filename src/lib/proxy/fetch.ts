5d6cb8145798ca75c244044a29089a6a0ce66905
Callum Gare <cgare@infoxchange.org>
2025-12-04 15:13:26 +1100

    fixup! feat: add proxy lib

--- src/lib/proxy/fetch.ts
+++ src/lib/proxy/fetch.ts
@@ -3,12 +3,22 @@ import {
   EnvHttpProxyAgent,
   fetch as undiciFetch,
 } from "undici";
+import { bootstrap } from "global-agent";
 
 export function setupProxyGlobally() {
+  // To cover libraries that use fetch
   // See https://nodejs.org/api/globals.html#custom-dispatcher
   // This might stop being needed at some point: https://github.com/actions/create-github-app-token/pull/143#discussion_r1747641337
   const envHttpProxyAgent = new EnvHttpProxyAgent();
   setGlobalDispatcher(envHttpProxyAgent);
+
+  // To cover libraries that use the http/https object
+  if (!process.env.GLOBAL_AGENT_HTTP_PROXY) {
+    process.env.GLOBAL_AGENT_HTTP_PROXY = process.env.HTTP_PROXY;
+    process.env.GLOBAL_AGENT_HTTPS_PROXY =
+      process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY;
+  }
+  bootstrap();
 }
 
 export function getProxiedFetch() {
