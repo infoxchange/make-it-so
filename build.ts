9da165902f958d533081c0fabcc1c31fe0a4ab2a
Callum Gare <cgare@infoxchange.org>
2026-01-20 15:46:53 +1100

    wip: draft

--- build.ts
+++ build.ts
@@ -3,6 +3,7 @@
 import * as esbuild from "esbuild";
 import { readFileSync, rmSync } from "fs";
 import { execSync } from "child_process";
+import { renameAwsPlugin } from "./esbuild-plugin-rename-aws.js";
 
 interface PackageJson {
   peerDependencies?: Record<string, string>;
@@ -15,31 +16,6 @@ const packageJson: PackageJson = JSON.parse(
 
 // Clean dist directory before building
 rmSync("dist", { recursive: true, force: true });
-
-// Plugin to rename 'aws' imports and variables to avoid conflicts
-const renameAwsPlugin: esbuild.Plugin = {
-  name: "rename-aws",
-  setup(build) {
-    build.onLoad({ filter: /\.ts$/ }, async (args) => {
-      const fs = await import("fs/promises");
-      let contents = await fs.readFile(args.path, "utf8");
-
-      // Rename namespace imports: import * as aws from "@pulumi/aws"
-      contents = contents.replace(
-        /import\s+\*\s+as\s+aws\s+from\s+["']@pulumi\/aws["']/g,
-        'import * as awsSdk from "@pulumi/aws"',
-      );
-
-      // Rename the usage of 'aws.' to 'awsSdk.'
-      contents = contents.replace(/\baws\./g, "awsSdk.");
-
-      return {
-        contents,
-        loader: "ts",
-      };
-    });
-  },
-};
 
 // Get all dependencies that should be external (not bundled)
 const external = [
