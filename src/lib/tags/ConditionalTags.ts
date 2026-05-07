ce7d9c65387fd43b6c75fe2ef65a2aeb15c91946
Callum Gare <cgare@infoxchange.org>
2026-05-07 15:05:26 +1000

    fix: setupTags conflict with SST

--- src/lib/tags/ConditionalTags.ts
+++ src/lib/tags/ConditionalTags.ts
@@ -1,4 +1,4 @@
-import { IAspect, Tags } from "aws-cdk-lib";
+import { Aspects, IAspect, Tag } from "aws-cdk-lib";
 import { IConstruct } from "constructs";
 
 export class ConditionalTags implements IAspect {
@@ -10,7 +10,9 @@ export class ConditionalTags implements IAspect {
 
   visit(node: IConstruct) {
     this.getTags(node)?.forEach(({ key, value }) => {
-      Tags.of(node).add(key, value);
+      // We need to use a priority greater than 500 avoid a conflict that occurs with some aspects created by SST.
+      // Since cdk's Tags aspect doesn't let us set a priority we have to use the slightly lower level Tag aspect here.
+      Aspects.of(node).add(new Tag(key, value), { priority: 600 });
     });
   }
 }
